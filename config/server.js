6d3d08af0e4f6139ab5755bb8579f2600ed5bbd3
JulioMontas <hello@juliomontas.com>
2021-07-08 16:42:54 -0400

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ed6ce97b8701928334cb7981b4c8d267'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'f65cc68d-19dd-4be0-80a9-5c65f6330ed1'
+};
